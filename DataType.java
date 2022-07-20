2feccf49be741a4cce44735d03e46ff9ddb2d369
Miral Patel <patelmv.2995@gmail.com>
2022-07-20 21:48:15 +0100

    DataType default

---
+++ DataType.java
@@ -0,0 +1,25 @@
+package DataType;
+
+public class DataType {
+    public static void main(String[] args) {
+        byte a=1;
+        short b=2;
+        long c=3;
+        int d=4;
+        float e=101.101f;
+        double f=111.111;
+        char g=6;
+        char h=5;
+        boolean i=true;
+        boolean j=false;
+
+        System.out.println("Addition= "+(a+b));
+        System.out.println("Subtraction= "+(c-d));
+        System.out.println("Multiplication= "+(e*f));
+        System.out.println("Division= "+(d/b));
+        System.out.println("Module= "+(a%c));
+        System.out.println(g+h);
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
